494acc79c3d65b7a1dbb24b75dc61f03a023ec49
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-10-02 23:17:59 -0400

    files set up, yarn added in root and client files, and part 1 complete.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-
 import SavedList from './Movies/SavedList';
+import { Route } from "react-router-dom"; 
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +13,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MoveList} />
+      <Route path="/movies:id" component={Movie} />
     </div>
   );
 };
